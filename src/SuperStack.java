f2322094d62c9cbeea226e56a5f7c0165af6fe18
willowarana <willow@ou.edu>
2020-10-21 16:32:49 -0500

    Make a stack

---
+++ src/SuperStack.java
@@ -0,0 +1,29 @@
+
+public class SuperStack<T> {
+	private T[] items;
+	private int top;
+	
+	public SuperStack<T>(int size) {
+		items = (T[]) new Object[size];
+	}
+	
+	public boolean isEmpty() {
+		//TODO
+		return true;
+	}
+	
+	public boolean isFull() {
+		//TODO
+		return true;
+	}
+	
+	public boolean push(T item) {
+		//TODO
+		return true;
+	}
+	
+	public T pop() {
+		//TODO
+		return null;
+	}
+}
