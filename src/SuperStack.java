07e74f50ce5071080a3811b5b52a9a53a6158d39
willowarana <willow@ou.edu>
2020-10-21 17:01:20 -0500

    Add implementation of push and isEmpty

--- src/SuperStack.java
+++ src/SuperStack.java
@@ -3,27 +3,33 @@ public class SuperStack<T> {
 	private T[] items;
 	private int top;
 	
-	public SuperStack<T>(int size) {
+	public SuperStack(int size) {
 		items = (T[]) new Object[size];
 	}
 	
-	public boolean isEmpty() {
-		//TODO
-		return true;
+	public boolean isEmpty() { 
+		return top == 0;
 	}
 	
 	public boolean isFull() {
-		//TODO
-		return true;
+		return top == items.length;
 	}
 	
 	public boolean push(T item) {
-		//TODO
+		if(isFull()) {
+			return false;
+		}
+		
+		items[top++] = item;
 		return true;
 	}
 	
 	public T pop() {
-		//TODO
-		return null;
+		if(isEmpty())
+			return null;
+		else {
+			top = top - 1;
+			return items[top];
+		}
 	}
 }
